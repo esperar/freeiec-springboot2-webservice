5b7d8cc354a88b0894caf4da6b5e1b8b27fb3ace
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-10 22:08:14 +0900

    PostsSaveRequestDto 등록 Api 생성

---
+++ src/main/java/com/example/springAWS/web/PostsApiContoller.java
@@ -0,0 +1,21 @@
+package com.example.springAWS.web;
+
+import com.example.springAWS.servicie.dto.PostsService;
+import com.example.springAWS.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiContoller {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+
+        return postsService.save(requestDto);
+    }
+}
