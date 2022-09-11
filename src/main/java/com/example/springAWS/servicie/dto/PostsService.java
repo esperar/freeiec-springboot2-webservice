11507ef97402003e9947e40d94aa032981160c8b
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-11 20:27:01 +0900

    게시글 전체 조회 화면 만들기 작성

--- src/main/java/com/example/springAWS/servicie/dto/PostsService.java
+++ src/main/java/com/example/springAWS/servicie/dto/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springAWS.servicie.dto;
 
 import com.example.springAWS.domain.posts.Posts;
 import com.example.springAWS.domain.posts.PostsRepository;
+import com.example.springAWS.web.dto.PostsListResponseDto;
 import com.example.springAWS.web.dto.PostsResponseDto;
 import com.example.springAWS.web.dto.PostsSaveRequestDto;
 import com.example.springAWS.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/springAWS/web/IndexController.java
+++ src/main/java/com/example/springAWS/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.example.springAWS.web;
 
+import com.example.springAWS.servicie.dto.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts" , postsService.findAllDesc());
         return "index";
     }
 }
